cc00e20e05e0d8ade7c680a96b8fadca7a5068b3
Durga-Rajan <31245621+Durga-Rajan@users.noreply.github.com>
2019-10-14 17:12:04 +0530

    Add files via upload

---
+++ Header - Customization/DesignerPageDock.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DockingManager_HeaderBackground
+{
+    /// <summary>
+    /// Interaction logic for DesignerPageDock.xaml
+    /// </summary>
+    public partial class DesignerPageDock : Window
+    {
+        public DesignerPageDock()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Header - Customization/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DockingManager_HeaderBackground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // DockingManager.SetAnimationDelay(dockingManager, new Duration(new TimeSpan(30000000)));
+            //combo1.ItemsSource = Enum.GetValues(typeof(VisualStyles)).Cast<VisualStyles>();
+            
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItemAdv item;
+            ComboBoxAdv combo = sender as ComboBoxAdv;
+            if (combo != null)
+            {
+                if (combo.SelectedItem != null)
+                {
+                    item = combo.SelectedItem as ComboBoxItemAdv;
+                    //SkinStorage.SetVisualStyle(dockingManager, item.Content.ToString());
+                    //SfSkinManager.SetVisualStyle(this, VisualStyles.SystemTheme);
+
+                }
+            }
+        }
+
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //SfSkinManager.SetVisualStyle(dockingManager, (VisualStyles)combo1.SelectedValue);
+        }
+    }
+
+    public class OrderInfo
+    {
+        int orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shippingCity;
+
+        public int OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; }
+        }
+
+        public string CustomerID
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public string ShipCity
+        {
+            get { return shippingCity; }
+            set { shippingCity = value; }
+        }
+
+        public OrderInfo(int orderId, string customerName, string country, string customerId, string shipCity)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+        }
+    }
+
+    public class ViewModel
+    {
+        private ObservableCollection<OrderInfo> _orders;
+
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+
+        public ViewModel()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo(1001, "Maria Anders", "Germany", "ALFKI", "Berlin"));
+            _orders.Add(new OrderInfo(1002, "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            _orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F."));
+            _orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London"));
+            _orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula"));
+            _orders.Add(new OrderInfo(1006, "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            _orders.Add(new OrderInfo(1007, "Frederique Cite aux", "France", "BLONP", "Strasbourg"));
+            _orders.Add(new OrderInfo(1008, "Martin", "Spain", "BOLID", "Madrid"));
+            _orders.Add(new OrderInfo(1009, "Laurence", "France", "BONAP", "Marseille"));
+            _orders.Add(new OrderInfo(1010, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen"));
+        }
+    }
+}
